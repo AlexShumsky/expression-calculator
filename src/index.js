24e1813e79fe8871fc67b7e262fa44bc887693a3
AlexShumsky <sanya.shumskiy.95@mail.ru>
2021-08-31 01:20:26 +0300

    feat: add demo-solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 function eval() {
     // Do not use eval!!!
     return;
-}
-
+}/*
+let a = 'c';
+let expr = ['a', '+', 'b'];
+let i = 1;
+expr.splice(i - 1, 3, a);
+console.log(expr);*/
 function expressionCalculator(expr) {
-    // write your solution here
+    let mainExpr = expr.split(' ').join('').split('');
+    console.log(mainExpr);
+    mainExpr.map((val, i) => { if (val === '*' || val === '/') mainExpr.splice(i - 1, 3, getResult(mainExpr[i - 1], mainExpr[i], mainExpr[i + 1])) });
+    console.log(mainExpr);
+    mainExpr.map((val, i) => { if (val === '+' || val === '-') mainExpr.splice(i - 1, 3, getResult(mainExpr[i - 1], mainExpr[i], mainExpr[i + 1])) });
+    return +mainExpr.join('');
+}
+function getResult(a, n, b) {
+    if (n === '/' && b == '0') throw Error("TypeError: Division by zero.");
+    return (n === '*') ? +a * +b :
+        (n === '/') ? +a / +b :
+            (n === '+') ? +a + +b :
+                (n === '-') ? +a - +b : '';
 }
 
 module.exports = {
